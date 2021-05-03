3db782833b890e21d198ae1077dd79e904a1589d
YuKai Lee <kevin991239@gmail.com>
2021-05-03 22:52:49 +0800

    feat(frontend): implementation GameDetail.
    GameCard.js: The cards on the deck.
    GameDetail.js: Render of the main game page.
    saboteur.scss: Added some styles related to the main game page.
    The page is just a draft and needs to be changed in the future.

--- mysite/frontend/src/containers/GameDetail.js
+++ mysite/frontend/src/containers/GameDetail.js
@@ -1,9 +1,10 @@
 import React, {Component} from "react";
 
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faUserFriends, faSignInAlt} from "@fortawesome/free-solid-svg-icons";
+import { faHammer, faLightbulb, faShoppingCart, faTrashAlt, faDoorOpen } from "@fortawesome/free-solid-svg-icons";
 import * as PropTypes from "prop-types";
+import GameCard from "../components/GameCard";
 
 class GameDetail extends Component {
     state = {
@@ -19,9 +20,158 @@ class GameDetail extends Component {
 
     render() {
         return (
-            <div>
-                {this.state.data}
-            </div>
+            <>
+                {/* room number */}
+                <div className="text-center">
+                    <span className="nametag roomnumber">Room: {this.state.data}</span>
+                </div>
+
+                {/* Deck */}
+                <Container className="my-3">
+                    <Row>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                    </Row>
+                    <Row>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                    </Row>
+                    <Row>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                    </Row>
+                    <Row>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                    </Row>
+                    <Row>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                        <GameCard></GameCard>
+                    </Row>
+                </Container>
+
+                {/* Rival name & status */}
+                <Container>
+                    <Row className="my-3">
+                        <Col className="col-4">
+                            <div className="nametag">Player1</div>
+                            <div className="d-flex justify-content-center">
+                                <span><FontAwesomeIcon className="tool" icon={faHammer}/></span>
+                                <span><FontAwesomeIcon className="tool" icon={faLightbulb}/></span>
+                                <span><FontAwesomeIcon className="tool" icon={faShoppingCart}/></span>
+                            </div>
+                            <div className="d-flex justify-content-center">
+                                <span><img className="handcard"/></span>
+                                <span><img className="handcard"/></span>
+                                <span><img className="handcard"/></span>
+                                <span><img className="handcard"/></span>
+                                <span><img className="handcard"/></span>
+                            </div>
+                        </Col>
+                        <Col className="col-4">
+                            <div className="nametag">Player2</div>
+                            <div className="d-flex justify-content-center">
+                                <span><FontAwesomeIcon className="tool" icon={faHammer}/></span>
+                                <span><FontAwesomeIcon className="tool" icon={faLightbulb}/></span>
+                                <span><FontAwesomeIcon className="tool" icon={faShoppingCart}/></span>
+                            </div>
+                            <div className="d-flex justify-content-center">
+                                <span><img className="handcard"/></span>
+                                <span><img className="handcard"/></span>
+                                <span><img className="handcard"/></span>
+                                <span><img className="handcard"/></span>
+                                <span><img className="handcard"/></span>
+                            </div>
+                        </Col>
+                        <Col className="col-4">
+                            <div className="nametag">Player3</div>
+                            <div className="d-flex justify-content-center">
+                                <span><FontAwesomeIcon className="tool" icon={faHammer}/></span>
+                                <span><FontAwesomeIcon className="tool" icon={faLightbulb}/></span>
+                                <span><FontAwesomeIcon className="tool" icon={faShoppingCart}/></span>
+                            </div>
+                            <div className="d-flex justify-content-center">
+                                <span><img className="handcard"/></span>
+                                <span><img className="handcard"/></span>
+                                <span><img className="handcard"/></span>
+                                <span><img className="handcard"/></span>
+                                <span><img className="handcard"/></span>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+
+                {/* Your name & status */}
+                <Container>
+                    <Row>
+                        <Col className="col-8">
+                            <Row>
+                                <button className="btn btn-primary" type="button">Jeffery : 好矮人</button>
+                            </Row>
+                        </Col>
+                        <Col className="col-4 align-self-center d-flex justify-content-around">
+                            <Row>
+                                <span><FontAwesomeIcon className="tool" icon={faHammer}/></span>
+                                <span><FontAwesomeIcon className="tool" icon={faLightbulb}/></span>
+                                <span><FontAwesomeIcon className="tool" icon={faShoppingCart}/></span>
+                                <span>$ : 0</span>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="col-8">
+                            <Row>
+                                <Col className="mycard"></Col>
+                                <Col className="mycard"></Col>
+                                <Col className="mycard"></Col>
+                                <Col className="mycard"></Col>
+                                <Col className="mycard"></Col>
+                            </Row>
+                        </Col>
+                        <Col className="col-4 align-self-center d-flex justify-content-around">
+                            <Row>
+                                <span><FontAwesomeIcon icon={faTrashAlt}/></span>
+                                <span><FontAwesomeIcon icon={faDoorOpen}/></span>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
         );
     }
 }
