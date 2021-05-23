529c71d0c2291d733ecca8a30d0da01bcf0400f0
Jeffery Ho <chiachun2491@gmail.com>
2021-05-24 00:21:23 +0800

    feat(backend): delete unused templates and views

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,8 +22,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('api/auth/', include('authentication.urls')),
-    path('api/game/', include('game.api_urls')),
-    path('game/', include('game.urls')),
+    path('api/game/', include('game.urls')),
     path('admin/', admin.site.urls),
     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
