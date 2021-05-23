4f7e0ef5dbcf8499bd1bd28bbc2f1f0bf4a3a844
Jeffery Ho <chiachun2491@gmail.com>
2021-05-23 23:34:00 +0800

    refactor(frontend): handleLogout Function use then()

--- mysite/frontend/src/components/Logout.js
+++ mysite/frontend/src/components/Logout.js
@@ -2,15 +2,18 @@ import axiosInstance from '../Api';
 
 function handleLogout() {
     try {
-        axiosInstance.post('/auth/blacklist/', {
-            refresh_token: localStorage.getItem('refresh_token'),
-        });
-        localStorage.removeItem('access_token');
-        localStorage.removeItem('refresh_token');
-        localStorage.removeItem('username');
-        axiosInstance.defaults.headers['Authorization'] = null;
-        console.log('Logout successful');
-        window.location.href = '/';
+        axiosInstance
+            .post('/auth/blacklist/', {
+                refresh_token: localStorage.getItem('refresh_token'),
+            })
+            .then(() => {
+                localStorage.removeItem('access_token');
+                localStorage.removeItem('refresh_token');
+                localStorage.removeItem('username');
+                axiosInstance.defaults.headers['Authorization'] = null;
+                console.log('Logout successful');
+                window.location.href = '/';
+            });
     } catch (e) {
         console.log(e);
     }
