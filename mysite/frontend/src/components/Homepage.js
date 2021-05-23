f806913fb213828dd8e785e72a08b3b74d702040
Jeffery Ho <chiachun2491@gmail.com>
2021-05-23 23:43:57 +0800

    fix(frontend): fix style to fit react JSX style

--- mysite/frontend/src/components/Homepage.js
+++ mysite/frontend/src/components/Homepage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Button } from 'react-bootstrap';
+import { Row, Col, Button, Image } from 'react-bootstrap';
 import { faGrinStars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import demo from '../images/homepage/demo.png';
@@ -9,7 +9,7 @@ const Home = () => {
         <>
             <Row className={'d-flex align-items-center'}>
                 <Col xs={'12'} md={'6'} className={'p-3 p-md-5 d-flex justify-content-center'}>
-                    <img src={demo} alt='Demo' class='img-fluid' />
+                    <Image src={demo} alt='Demo' fluid={true} />
                 </Col>
                 <Col xs={'12'} md={'6'} className={'pt-3 pb-5 px-5 p-md-5'}>
                     <h1>矮人礦坑</h1>
